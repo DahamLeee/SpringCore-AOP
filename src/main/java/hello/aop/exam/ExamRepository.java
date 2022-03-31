b128d28c44dc1cc13bf6ad25b81260a7c8a18a9b
DahamLeee <dlek1011@gmail.com>
2022-03-31 18:57:02 +0900

    Custom RetryAspect

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+    @Retry(value = 4) // 이거 횟수 제한 있어야 해 ㅋㅋ 안그러면 무제한 요청과 셀프 디도스가 되어버릴 수도 있어..
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
